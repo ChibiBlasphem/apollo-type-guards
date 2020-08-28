0b0fbfd26f82bc2014830a1bcebaac3f65cc8b77
Christopher Debove <chibiblasphem@gmail.com>
2020-08-28 08:50:56 +0200

    test: split the only parser test into 3 tests

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -6,14 +6,18 @@ const postFixture = loadFixture('parser/PostQuery')
 const unionedFixture = loadFixture('parser/UnionedQuery')
 
 describe('extractGraphQLTypes', () => {
-  it('Should return an array of the types containing a property "__typename"', () => {
+  it('Should return an array of the interfaces containing a property "__typename"', () => {
     expect(extractGraphQLTypes(userFixture)).toEqual([
       { name: 'CurrentUserQuery_currentUser', typename: 'User' },
     ])
+  })
+  it('Should return an array of the types containing a property "__typename"', () => {
     expect(extractGraphQLTypes(postFixture)).toEqual([
       { name: 'PostQuery_post_comments', typename: 'Comment' },
       { name: 'PostQuery_post', typename: 'Post' },
     ])
+  })
+  it('Should return multiple items if "__typename" is an String Union', () => {
     expect(extractGraphQLTypes(unionedFixture)).toEqual([
       { name: 'UnionedQuery_search', typename: 'User' },
       { name: 'UnionedQuery_search', typename: 'Repository' },
