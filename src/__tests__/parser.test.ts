e01e98c215e943335d217ef6104dc90ade0dc389
Christopher Debove <chibiblasphem@gmail.com>
2020-08-28 02:50:35 +0200

    feat: add union management

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -3,6 +3,7 @@ import { extractGraphQLTypes } from '../parser'
 
 const userFixture = loadFixture('parser/CurrentUserQuery')
 const postFixture = loadFixture('parser/PostQuery')
+const unionedFixture = loadFixture('parser/UnionedQuery')
 
 describe('extractGraphQLTypes', () => {
   it('Should return an array of the types containing a property "__typename"', () => {
@@ -12,6 +13,10 @@ describe('extractGraphQLTypes', () => {
     expect(extractGraphQLTypes(postFixture)).toEqual([
       { name: 'PostQuery_post_comments', typename: 'Comment' },
       { name: 'PostQuery_post', typename: 'Post' },
+    ])
+    expect(extractGraphQLTypes(unionedFixture)).toEqual([
+      { name: 'UnionedQuery_search', typename: 'User' },
+      { name: 'UnionedQuery_search', typename: 'Repository' },
     ])
   })
 })
--- src/parser.ts
+++ src/parser.ts
@@ -13,7 +13,14 @@ export const extractGraphQLTypes = (code: string): GraphQLTypeInfo[] => {
   const namedExports = extractNamedExports(program.body)
   const typeDeclarations = extractDeclarations(namedExports)
 
-  return typeDeclarations.filter(hasTypenameProperty).map(transformToGraphQLTypeInfo)
+  const graphqlTypeInfos: GraphQLTypeInfo[] = []
+  const validDeclarations = typeDeclarations.filter(hasTypenameProperty)
+
+  for (let i = 0, l = validDeclarations.length; i < l; ++i) {
+    graphqlTypeInfos.push(...transformToGraphQLTypeInfo(validDeclarations[i]))
+  }
+
+  return graphqlTypeInfos
 }
 
 const extractNamedExports = (statements: types.Statement[]): types.ExportNamedDeclaration[] => {
@@ -53,19 +60,28 @@ const hasTypenameProperty = (type: TSTypeDeclaration): boolean => {
 const isTypenameProperty = (
   property: types.TSTypeElement
 ): property is types.TSPropertySignature => {
-  return (
+  if (
     types.isTSPropertySignature(property) &&
     types.isIdentifier(property.key) &&
     property.key.name === GRAPHQL_OBJECT_PROPERTY &&
-    types.isTSTypeAnnotation(property.typeAnnotation) &&
-    types.isTSLiteralType(property.typeAnnotation.typeAnnotation) &&
-    types.isStringLiteral(property.typeAnnotation.typeAnnotation.literal)
-  )
+    types.isTSTypeAnnotation(property.typeAnnotation)
+  ) {
+    return (
+      (types.isTSLiteralType(property.typeAnnotation.typeAnnotation) &&
+        types.isStringLiteral(property.typeAnnotation.typeAnnotation.literal)) ||
+      (types.isTSUnionType(property.typeAnnotation.typeAnnotation) &&
+        property.typeAnnotation.typeAnnotation.types.every(
+          type => types.isTSLiteralType(type) && types.isStringLiteral(type.literal)
+        ))
+    )
+  }
+
+  return false
 }
 
 const extractTypenameValue = (
   typeAnnotation: types.TSTypeAnnotation | null
-): string | undefined => {
+): string | string[] | undefined => {
   if (
     typeAnnotation &&
     types.isTSTypeAnnotation(typeAnnotation) &&
@@ -74,9 +90,28 @@ const extractTypenameValue = (
   ) {
     return typeAnnotation.typeAnnotation.literal.value
   }
+
+  if (
+    typeAnnotation &&
+    types.isTSTypeAnnotation(typeAnnotation) &&
+    types.isTSUnionType(typeAnnotation.typeAnnotation)
+  ) {
+    const values: string[] = [],
+      unionType = typeAnnotation.typeAnnotation.types
+    for (let i = 0, l = unionType.length; i < l; ++i) {
+      const type = unionType[i]
+      if (types.isTSLiteralType(type) && types.isStringLiteral(type.literal)) {
+        values.push(type.literal.value)
+      }
+    }
+
+    if (values.length === unionType.length) {
+      return values
+    }
+  }
 }
 
-const extractTypename = (type: TSTypeDeclaration): string => {
+const extractTypename = (type: TSTypeDeclaration): string | string[] => {
   let typenameProperty: types.TSPropertySignature | undefined
   if (types.isTSInterfaceDeclaration(type)) {
     typenameProperty = type.body.body.find(isTypenameProperty)
@@ -84,7 +119,7 @@ const extractTypename = (type: TSTypeDeclaration): string => {
     typenameProperty = (type.typeAnnotation as types.TSTypeLiteral).members.find(isTypenameProperty)
   }
 
-  let typename: string | undefined
+  let typename: string | string[] | undefined
   if (!typenameProperty || !(typename = extractTypenameValue(typenameProperty.typeAnnotation))) {
     throw new Error(
       `Type ${type.id.name} should have a property ${GRAPHQL_OBJECT_PROPERTY} which has a const string value`
@@ -94,6 +129,10 @@ const extractTypename = (type: TSTypeDeclaration): string => {
   return typename
 }
 
-const transformToGraphQLTypeInfo = (type: TSTypeDeclaration): GraphQLTypeInfo => {
-  return { name: type.id.name, typename: extractTypename(type) }
+const transformToGraphQLTypeInfo = (type: TSTypeDeclaration): GraphQLTypeInfo[] => {
+  const typename = extractTypename(type),
+    name = type.id.name
+  return Array.isArray(typename)
+    ? typename.map(typename => ({ name, typename }))
+    : [{ name, typename }]
 }
